4b0a68f7aecf793799af82e860bd32b24b833d16
Sindarkella <sssindarkella@gmail.com>
2024-10-04 23:12:45 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string =  input('Ваш любимый вид спорта ')
+print('Количество символов: ',len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+first_symbol = my_string[0]
+last_symbol = my_string[-1]
+print('Первая буква: ',first_symbol)
+print('Последняя буква: ',last_symbol)
